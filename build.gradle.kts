92266dc23fb60d68169483c3fda3963de2d2b9fb
Ym_gg <38462592+YmggDEV@users.noreply.github.com>
2021-02-11 12:43:29 +0800

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    val kotlinVersion = "1.4.21"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.serialization") version kotlinVersion
+
+    id("net.mamoe.mirai-console") version "2.0-RC" // mirai-console version
+}
+
+mirai {
+    coreVersion = "2.0-RC" // mirai-core version
+}
+
+group = "org.example"
+version = "0.1.0"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    jcenter()
+    maven("https://dl.bintray.com/kotlin/kotlin-eap")
+}
