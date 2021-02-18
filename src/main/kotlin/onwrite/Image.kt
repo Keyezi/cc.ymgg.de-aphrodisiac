d449be627efbbc0d2ffd993c09f2fe0efd650f55
Ym_gg <cqpro@qq.com>
2021-02-18 23:06:21 +0800

    TODO获取图片信息

---
+++ src/main/kotlin/onwrite/Image.kt
@@ -0,0 +1,8 @@
+package cc.ymgg.deaphrodisac.onwrite
+
+import net.mamoe.mirai.message.data.Image
+
+//todo:可能自行添加获取图片大小长宽
+fun Image.getheight() {}
+fun Image.getweight() {}
+fun Image.getsize() {}
